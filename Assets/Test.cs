475df6b216455d4224c6f54d5fac754d47b7d7a9
鬼海勝巳 <k-kikai@s4.dion.ne.jp>
2016-11-21 20:17:09 +0900

    Lesson4 スクリプト基礎　課題提出
    Assets/Tesst.csの追加

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Boss
 {
+    // Magic関数
     public int mp = 53;
     public void Magic()
     {
@@ -19,23 +20,30 @@ public class Boss
 }
 public class Test : MonoBehaviour
 {
+    // 配列arrayの宣言
     int[] array = {10,30,60,80,65}; 
+    
     // Use this for initialization
     void Start()
     {
+        //　要素値の表示
         for (int i= 0; i < 5; i++)
         {
             Debug.Log(array[i]);
         }
+        //　要素値の逆順表示
         for(int i =4;i>=0;i--)
         {
             Debug.Log(array[i]);
         }
+
+        // Magic関数を10回使用
         Boss lastboss = new Boss();
         for (int j = 0; j < 10; j++)
         {
             lastboss.Magic();
         }
+        // Magic関数を10回使った後さらに使用
         lastboss.Magic();
     }
 	
